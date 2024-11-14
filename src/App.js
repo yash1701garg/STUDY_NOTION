fd0d943cc4aceb603bc8442fe80d00588d5e7df3
yash1701garg <yashgarg1701@gmail.com>
2024-11-14 21:12:58 +0530

    navbar

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import './App.css';
 import Home from './pages/Home';
 import Signup from './pages/Signup.jsx'
 import Login from './pages/Login.jsx'
+import Navbar from './components/common/Navbar.jsx';
 
 function App() {
   return (
    <div className='w-screen min-h-screen bg-richblack-900 flex flex-col font-inter'>
+    <Navbar/>
     <Routes>
       <Route path='/' element={<Home/>}/>
       <Route path='/signup' element={<Signup/>}/>
